0901aeff1aa680bd1ef5f691705607dfbb8e1ae6
VuXuanTai <taivu.xd.k64@gmail.com>
2022-10-09 09:07:56 -0700

    submit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
 import { Home } from './components/JSX/JSX1';
 import { List } from './components/JSX/JSX2';
 import { Profile } from './components/JSX/JSX3';
+import { Login } from './components/JSX/JSX4';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Profile />
+  <Login />
 );
 
 // If you want to start measuring performance in your app, pass a function
